c21563847a003ffdd3f52a1cc734ac60c68fb758
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-04-17 20:02:56 +0700

    feat: add /api endpoint with welcome response

--- api/src/index.js
+++ api/src/index.js
@@ -5,6 +5,16 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
+app.get("/api", (req, res) => {
+  res.json({
+    success: true,
+    message: "Welcome API Cholestify",
+    metadata: {
+      status: 200,
+    },
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running in http://localhost:${PORT}`);
 });
