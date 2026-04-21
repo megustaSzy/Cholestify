a8b24bd1993d976f3af50f7bebadd44cbcc85706
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-04-21 13:28:43 +0700

    feat(user): add CRUD service for user with Prisma

--- api/src/services/user.service.js
+++ api/src/services/user.service.js
@@ -1,6 +1,28 @@
 import { prisma } from "../lib/prisma.js";
 
 export const UserService = {
+  async createUser(req, res) {
+    const newUser = req.body;
+
+    const data = await prisma.user.create({
+      data: {
+        nama: newUser.nama,
+        email: newUser.email,
+        password: newUser.password,
+        notelp: newUser.notelp,
+      },
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Data User Berhasil Ditambahkan",
+      data: data,
+      metadata: {
+        status: 201,
+      },
+    });
+  },
+
   async getUsers(req, res) {
     const data = await prisma.user.findMany({
       orderBy: {
@@ -8,13 +30,94 @@ export const UserService = {
       },
     });
 
-    res.json({
+    if (data.length === 0) {
+      return res.status(404).json({
+        success: false,
+        message: "Data User Tidak Ditemukan",
+        metadata: {
+          status: 404,
+        },
+      });
+    }
+
+    return res.json({
       success: true,
-      message: "Data User Berhasil Ditemukan",
+      message: process.env.USER_SUCCESS_MESSAGE,
       metadata: {
         status: 200,
       },
       data: data,
+    });
+  },
+
+  async getUsersById(req, res) {
+    const id = Number(req.params.id);
+
+    const data = await prisma.user.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "Data User Tidak Ditemukan",
+        metadata: {
+          status: 404,
+        },
+      });
+    }
+
+    return res.json({
+      success: true,
+      message: process.env.USER_SUCCESS_MESSAGE,
+      metadata: {
+        status: 200,
+      },
+      data: data,
+    });
+  },
+
+  async updateUser(req, res) {
+    const id = Number(req.params.id);
+
+    const updateUser = req.body;
+
+    const data = await prisma.user.findFirst({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!data) {
+      return res.status(404).json({
+        success: false,
+        message: "Data User Tidak Ditemukan",
+        metadata: {
+          status: 404,
+        },
+      });
+    }
+
+    await prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        nama: updateUser.nama,
+        email: updateUser.email,
+        password: updateUser.password,
+        notelp: updateUser.notelp,
+      },
+    });
+
+    return res.json({
+      success: true,
+      message: "Data User Berhasil Diubah",
+      metadata: {
+        status: 200,
+      },
     });
   },
 };
