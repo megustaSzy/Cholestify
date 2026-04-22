1a513b858ba9c522ebc323815de7f78519fc7f8c
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-04-22 22:29:57 +0700

    refactor: use HttpStatus constant in notExist utility

---
+++ api/src/utils/notExist.js
@@ -0,0 +1,14 @@
+import { HttpStatus } from "../constants/httpStatus.js";
+import { createError } from "./createError.js";
+
+export const notExist = async (prismaModel, where, message) => {
+  const data = await prismaModel.findFirst({
+    where,
+  });
+
+  if (!data) {
+    throw createError(HttpStatus.NOT_FOUND, message);
+  }
+
+  return data;
+};
