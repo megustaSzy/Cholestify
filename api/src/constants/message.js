96bdedecde44511c3448649e02c72328a95c215f
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-04-24 20:55:59 +0700

    feat: add consttant message.js

---
+++ api/src/constants/message.js
@@ -0,0 +1,26 @@
+export const MESSAGE = {
+  COMMON: {
+    BAD_REQUEST: "Parameter ID / Angka tidak Valid",
+    UPDATE: "Data Berhasil Diperbarui",
+  },
+  USER: {
+    SUCCESS: "Data User Berhasil Ditemukan",
+    NOT_FOUND: "Data User tidak Ditemukan",
+    DELETE: "Data User berhasil Dihapus",
+    EMAIL_EXIST: "Email sudah digunakan",
+  },
+  AUTH: {
+    LOGIN_SUCCESS: "Login berhasil",
+    LOGIN_FAILED: "Email atau password salah",
+    REGISTRASI_SUCCESS: "Registrasi Berhasil",
+    LOGOUT_SUCCESS: "Logout berhasil",
+    UNAUTHORIZED: "User tidak terautentikasi",
+    FORBIDDEN: "Akses ditolak",
+  },
+  TOKEN: {
+    NOT_FOUND: "Token tidak ditemukan",
+    INVALID: "Token tidak valid",
+    REFRESH_NOT_FOUND: "Refresh token tidak ditemukan",
+    REFRESH_INVALID: "Refresh token tidak valid",
+  },
+};
