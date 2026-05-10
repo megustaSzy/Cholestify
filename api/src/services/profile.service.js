b719d7d5dae496012cfd6b967d220dce7c868d88
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-10 22:06:28 +0700

    feat(profile): add getProfileById service method

--- api/src/services/profile.service.js
+++ api/src/services/profile.service.js
@@ -1,7 +1,10 @@
+import { MESSAGE } from "../constants/message.constant.js";
+import { NotFoundError } from "../exceptions/NotFoundError.js";
 import { prisma } from "../lib/prisma.js";
+import { badRequestId } from "../utils/bad-request-id.util.js";
 
 export const ProfileService = {
-  async findAll() {
+  async getProfiles() {
     const data = await prisma.profile.findMany({
       orderBy: {
         id: "desc",
@@ -9,5 +12,27 @@ export const ProfileService = {
     });
 
     return data;
+  },
+
+  async getProfileById(id) {
+    badRequestId(id, MESSAGE.COMMON.BAD_REQUEST);
+
+    const data = await prisma.profile.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    if (!data) {
+      throw NotFoundError(MESSAGE.PROFILE.NOT_FOUND);
+    }
+
+    return data;
+  },
+
+  async editProfile(id, body) {
+    badRequestId(id, MESSAGE.COMMON.BAD_REQUEST);
+
+    
   },
 };
