0c26b218a29b90e5f6400fe5e3ce9a2bef2117c7
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-11 13:05:45 +0700

    feat(profile): add getMyProfile and updateMyProfile service

--- api/src/services/profile.service.js
+++ api/src/services/profile.service.js
@@ -1,7 +1,9 @@
 import { MESSAGE } from "../constants/message.constant.js";
+import { ROLE } from "../constants/role.constant.js";
 import { NotFoundError } from "../exceptions/NotFoundError.js";
 import { prisma } from "../lib/prisma.js";
 import { badRequestId } from "../utils/bad-request-id.util.js";
+import { notExist } from "../utils/not-exist.util.js";
 
 export const ProfileService = {
   async getProfiles() {
@@ -14,6 +16,30 @@ export const ProfileService = {
     return data;
   },
 
+  async getMyProfile(user) {
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId: user.id,
+      },
+      include: {
+        user: {
+          select: {
+            id: true,
+            nama: true,
+            email: true,
+            notelp: true,
+          },
+        },
+      },
+    });
+
+    if (!profile) {
+      throw new NotFoundError(MESSAGE.PROFILE.NOT_FOUND);
+    }
+
+    return profile;
+  },
+
   async getProfileById(id) {
     badRequestId(id, MESSAGE.COMMON.BAD_REQUEST);
 
@@ -24,15 +50,67 @@ export const ProfileService = {
     });
 
     if (!data) {
-      throw NotFoundError(MESSAGE.PROFILE.NOT_FOUND);
+      throw new NotFoundError(MESSAGE.PROFILE.NOT_FOUND);
     }
 
     return data;
   },
 
-  async editProfile(id, body) {
+  async editProfile(id, body, user) {
     badRequestId(id, MESSAGE.COMMON.BAD_REQUEST);
 
-    
+    await notExist(prisma.profile, { id }, MESSAGE.PROFILE.NOT_FOUND);
+
+    const updateData = {};
+
+    // hanya update jika field dikirim
+    if (body.dob !== undefined) {
+      updateData.dob = new Date(body.dob);
+    }
+
+    if (body.bloodType !== undefined) {
+      updateData.bloodType = body.bloodType;
+    }
+
+    // admin boleh update isActive
+    if (user.role === ROLE.ADMIN && body.isActive !== undefined) {
+      updateData.isActive = body.isActive;
+    }
+
+    await prisma.profile.update({
+      where: {
+        id,
+      },
+      data: updateData,
+    });
+  },
+
+  async updateMyProfile(user, body) {
+    const profile = await prisma.profile.findUnique({
+      where: {
+        userId: user.id,
+      },
+    });
+
+    if (!profile) {
+      throw new NotFoundError(MESSAGE.PROFILE.NOT_FOUND);
+    }
+
+    const updateData = {};
+
+    if (body.dob !== undefined) {
+      updateData.dob = new Date(body.dob);
+    }
+
+    if (body.bloodType !== undefined) {
+      updateData.bloodType = body.bloodType;
+    }
+
+    await prisma.profile.update({
+      where: {
+        userId: user.id,
+      },
+      data: updateData,
+    });
   },
 };
