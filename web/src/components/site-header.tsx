c66e9d745d967711d1b97d7be1b7a59c18d4a788
Deni Himawan <denihimawan595@gmail.com>
2026-05-12 22:50:58 +0700

    refactor: update siteheader

--- web/src/components/site-header.tsx
+++ web/src/components/site-header.tsx
@@ -20,7 +20,7 @@ export function SiteHeader() {
   //   return "Dashboard";
   // };
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height) bg-white">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator
