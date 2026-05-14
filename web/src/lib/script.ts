bbd7c776d272caf331baab15d949465e218f89cc
Deni Himawan <denihimawan595@gmail.com>
2026-05-14 23:14:14 +0700

    feat: add function format date Indo

--- web/src/lib/script.ts
+++ web/src/lib/script.ts
@@ -1,3 +1,14 @@
 export const filterNotelp = (value: string) => {
   return value.replace(/[^0-9]/g, "");
 };
+
+// format Tanggal Indo
+export const formatDate = (tanggal: string, waktu?: string) => {
+  const date = new Date(tanggal).toLocaleDateString("id-ID", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  if (!waktu) return date;
+  return `${date} pukul ${waktu}`;
+};
