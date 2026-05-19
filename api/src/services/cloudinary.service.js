5d1ef765df6b1d850a61aea883d18c29fa1a5afe
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-19 17:21:28 +0700

    refactor: optimize Cloudinary upload stream pipeline and folder naming

--- api/src/services/cloudinary.service.js
+++ api/src/services/cloudinary.service.js
@@ -2,19 +2,21 @@ import cloudinary from "../config/cloudinary.js";
 
 export const uploadToCloudinary = (buffer) => {
   return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream(
-      {
-        folder: "cholestify/screenings",
-      },
-      (error, result) => {
-        if (error) {
-          return reject(error);
-        }
+    cloudinary.uploader
+      .upload_stream(
+        {
+          folder: "uploads",
+        },
+        (error, result) => {
+          if (error) {
+            console.log("Cloudinary Error:", error);
 
-        resolve(result);
-      },
-    );
+            return reject(error);
+          }
 
-    stream.end(buffer);
+          resolve(result);
+        },
+      )
+      .end(buffer);
   });
 };
