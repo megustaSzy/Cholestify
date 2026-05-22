fbc54dfaa819f25213f0bc4d55d957b9b8131e9c
Raditya Ahmad <raditiaahmad50@gmail.com>
2026-05-22 22:13:17 +0700

    feat(server): initialize HTTP server and Socket.io for websocket connections

--- api/src/server.js
+++ api/src/server.js
@@ -1,7 +1,26 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
 import app from "./index.js";
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server running in http://localhost:${PORT}`);
+const httpServer = createServer(app);
+
+export const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(`[WebSocket] Client Connected: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`[WebSocket] Client Disconnected: ${socket.id}`);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Server & WebSocket running in http://localhost:${PORT}`);
 });
