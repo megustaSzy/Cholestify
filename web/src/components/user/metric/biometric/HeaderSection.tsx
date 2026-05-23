867506c8a4ea1c19ffc082b980e31479c1bf4f38
Deni Himawan <denihimawan595@gmail.com>
2026-05-23 15:23:50 +0700

    feat (log biometrics): rename file and update text

--- web/src/components/user/metric/biometric/HeaderSection.tsx
+++ web/src/components/user/metric/biometric/HeaderSection.tsx
@@ -4,7 +4,7 @@ export default function HeaderSection() {
   return (
     <div className="mb-4 md:mb-6 text-start">
       <h1 className="text-xl sm:text-2xl font-bold text-foreground">
-        Log Biometrics
+        Data Biometrics
       </h1>
       <p className="text-xs sm:text-sm text-muted-foreground mt-1">
         Masukkan pengukuran fisik terbaru Anda untuk memastikan profil klinis
