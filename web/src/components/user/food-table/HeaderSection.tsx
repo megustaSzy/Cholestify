f53bf516a2529d455691b9ba577aa2a30f115710
Deni Himawan <denihimawan595@gmail.com>
2026-05-23 15:21:37 +0700

    feat (food table): rename file and update text

--- web/src/components/user/food-table/HeaderSection.tsx
+++ web/src/components/user/food-table/HeaderSection.tsx
@@ -4,7 +4,7 @@ export default function HeaderSection() {
   return (
     <div className="mb-4 md:mb-6 text-start">
       <h1 className="text-xl sm:text-2xl font-bold text-foreground">
-        Food Table
+        Tabel Makanan
       </h1>
       <p className="text-xs sm:text-sm text-muted-foreground mt-1">
         Berikut tabel makanan yang dikategorikan berdasarkan dampaknya
