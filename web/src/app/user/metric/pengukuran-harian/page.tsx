db01e0a59afd163dab9f0f080a323c86641a5ea7
Deni Himawan <denihimawan595@gmail.com>
2026-05-25 09:39:57 +0700

    feat: add Skeleton Daily Tracking

--- web/src/app/user/metric/pengukuran-harian/page.tsx
+++ web/src/app/user/metric/pengukuran-harian/page.tsx
@@ -2,9 +2,10 @@ import { AppSidebar } from "@/components/AppSidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import { Metadata } from "next";
-import React from "react";
+import React, { Suspense } from "react";
 import HeaderSection from "@/components/user/metric/daily-tracking/HeaderSection";
 import DailyTrackingContent from "@/components/user/metric/daily-tracking/DailyTrackingContent";
+import { DailyTrackingPageSkeleton } from "@/components/user/metric/daily-tracking/DailyTrackingSkeleton";
 
 export const metadata: Metadata = {
   title: "Cholestify - Daily Tracking",
@@ -30,8 +31,10 @@ export default function DailyTrackingPage() {
             <div className="flex flex-col gap-4 py-6 md:gap-6 md:py-8">
               <div className="w-full px-4 sm:px-6 lg:px-8">
                 <div className="w-full max-w-6xl mx-auto flex flex-col gap-6">
-                  <HeaderSection />
-                  <DailyTrackingContent />
+                  <Suspense fallback={<DailyTrackingPageSkeleton />}>
+                    <HeaderSection />
+                    <DailyTrackingContent />
+                  </Suspense>
                 </div>
               </div>
             </div>
