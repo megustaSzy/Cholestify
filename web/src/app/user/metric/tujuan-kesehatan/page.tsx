d2a660e83988782ef8fad1c86bd43e50ad113ad3
Deni Himawan <denihimawan595@gmail.com>
2026-05-25 09:40:27 +0700

    feat: add Skeleton Set Health Goals Page

--- web/src/app/user/metric/tujuan-kesehatan/page.tsx
+++ web/src/app/user/metric/tujuan-kesehatan/page.tsx
@@ -3,6 +3,8 @@ import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import HeaderSection from "@/components/user/metric/set-health-goals/HeaderSection";
 import SetHealthGoalsSection from "@/components/user/metric/set-health-goals/SetHealthGoalsSection";
+import { SetHealthGoalsPageSkeleton } from "@/components/user/metric/set-health-goals/SetHealthGoalsSkeleton";
+import { Suspense } from "react";
 
 export default function SetHealthGoalsPage() {
   return (
@@ -22,8 +24,10 @@ export default function SetHealthGoalsPage() {
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
               <div className="px-4 sm:px-6 lg:px-8">
                 <div className="mx-auto flex w-full max-w-7xl flex-col gap-6">
-                  <HeaderSection />
-                  <SetHealthGoalsSection />
+                  <Suspense fallback={<SetHealthGoalsPageSkeleton />}>
+                    <HeaderSection />
+                    <SetHealthGoalsSection />
+                  </Suspense>
                 </div>
               </div>
             </div>
