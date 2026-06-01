1b1496f7dbbd61d9cd590b840d7583d2820598c4
Deni Himawan <denihimawan595@gmail.com>
2026-06-01 15:17:13 +0700

    feat: update nav get token

--- web/src/components/homepage/NavHeader.tsx
+++ web/src/components/homepage/NavHeader.tsx
@@ -1,11 +1,13 @@
 import { cookies } from "next/headers";
 import NavHeaderClient from "./NavHeaderClient";
 
-const TOKEN_COOKIE_NAME = "accessToken";
-
 export default async function NavHeader() {
   const cookieStore = await cookies();
-  const token = cookieStore.get(TOKEN_COOKIE_NAME)?.value;
 
-  return <NavHeaderClient hasToken={Boolean(token)} />;
+  const accessToken = cookieStore.get("accessToken")?.value;
+  const refreshToken = cookieStore.get("refreshToken")?.value;
+
+  const hasAuthCookie = Boolean(accessToken || refreshToken);
+
+  return <NavHeaderClient hasToken={hasAuthCookie} />;
 }
